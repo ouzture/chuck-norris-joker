784c9312203522612c4e8d284ee71494d65309d6
ouzture <ouzture@gmail.com>
2020-03-21 10:07:08 +0300

    Change ChuckNorrisQuotes to xml configured beanfrom Spring Java Based  Configuration

--- src/main/java/ouzture/springframework/chucknorrisjoker/ChuckNorrisJokerApplication.java
+++ src/main/java/ouzture/springframework/chucknorrisjoker/ChuckNorrisJokerApplication.java
@@ -2,8 +2,11 @@ package ouzture.springframework.chucknorrisjoker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class ChuckNorrisJokerApplication {
 
     public static void main(String[] args) {
